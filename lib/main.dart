e816fac12226d4bcd27db45a1b207bf1328e6b01
Syed Mohammad Abuzar <sabuzar00@gmail.com>
2023-09-17 13:17:39 +0500

    setup

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:schedulling_app/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.amberAccent,
+        brightness: Brightness.light
+      ),
+      darkTheme: ThemeData(
+
+      ),
+      home: const HomePage()
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Column(
+        children: [
+          Text(
+              "Theme Data",
+            style: TextStyle(
+              fontSize: 30
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
